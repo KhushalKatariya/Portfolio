e11291fef967cfdd6e5b1967587a22554f26b582
KhushalKatariya <68215936+KhushalKatariya@users.noreply.github.com>
2024-04-16 23:30:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ var typed = new Typed(".auto-type", {
     backDelay: 1000,
     loop: true
 })
+
+// function navBar(){
+//     let sidebar=document.getElementById("sidebar")
+//     let btn=document.querySelector(".toggle-btn")
+//     if(sidebar.style.right === "0px"){
+//         sidebar.style.right-"-250px";
+//         btn.classList.remove=("open")
+//     } else{
+//         sidebar.style.right = "0px"
+//         btn.classList.add("open");
+//     }
+// }
+function navBar(){
+    let sidebar = document.getElementById("sidebar");
+    let toggleBtn = document.querySelector(".toggle-btn");
+    if (sidebar.style.right === "0px") {
+        sidebar.style.right = "-250px";
+        toggleBtn.classList.remove("open");
+    } else {
+        sidebar.style.right = "0px";
+        toggleBtn.classList.add("open");
+    }
+}
